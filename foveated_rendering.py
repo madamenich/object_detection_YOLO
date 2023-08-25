1426adbf254ab24cc8b72539cada8d3aa9fb36df
madamenich <huotnich@pukyong.ac.kr>
2023-08-25 10:54:34 +0900

    find metric

--- foveated_rendering.py
+++ foveated_rendering.py
@@ -8,10 +8,10 @@ import xml.etree.ElementTree as ET
 # Load the image
 
 img = cv2.imread("image.png")
-ground_boxes = load_ground_truth(img,"ground_truth.xml")
-h, w,rows,cols = apply_grid(img, num_grid=4)
+# ground_boxes = load_ground_truth(img,"ground_truth.xml")
+h, w,rows,cols = apply_grid(img, num_grid=8)
 
-print('grdound',ground_boxes)
+# print('grdound',ground_boxes)
 
 # Define true positives, false positives, and false negatives
 tp = 0
@@ -19,15 +19,10 @@ fp = 0
 fn = 0
 
 
-# -----------------------------------------------------------------
-
-prediction_box =[]
-
-
 
 # Define the zoom factor for the hovered grid
 zoom_factor = 1
-# Define a variable to store the zoomed-in image
+# Define a variable to store the zoqqomed-in image
 zoomed_in_img = None
 # Define a global variable to store the current cell
 current_cell = None
@@ -41,12 +36,12 @@ import torch
 device = '0' if torch.cuda.is_available() else 'cpu'
 
 print('Running on %s' % device)
-
+counts =[]
 # Define a function to handle mouse events
 def mouse_event(event, x, y, flags, param):
     e1 = cv2.getTickCount()
     # Access the global variable
-    global current_cell, last_predicted_cell, prediction_box, tp, fp, fn
+    global current_cell, last_predicted_cell, prediction_box, tp, fp, fn, counts
 
     if event == cv2.EVENT_MOUSEMOVE:
         # Calculate the grid index based on the mouse position
@@ -88,29 +83,13 @@ def mouse_event(event, x, y, flags, param):
             results = model.predict(zoomed_in_img, classes=0, imgsz=width, device=device, augment =True)
             #TODO: draw the bounding boxes on the zoomed-in image
             zoomed_in_img = results[0].plot()
+            counts.append(results[0].boxes.shape[0])
             zoomed_in_img = cv2.resize(zoomed_in_img, (width, height), interpolation=cv2.INTER_NEAREST)
             blurred_img[grid_y1:grid_y2, grid_x1:grid_x2] = zoomed_in_img
-
-
-            boxes = results[0].boxes.xyxy.cpu().numpy()
-            print('boxes',boxes)
-            # for box in boxes:
-            #     prediction_box.append(box)
             cv2.imshow("Grid", blurred_img)
             cv2.imwrite(f"fvresult{cols}.png", blurred_img)
-            #TODO: calculate the IoU between the predicted bounding boxes and the ground truth bounding boxes
-            #Step1 scale the ground truth boxes
-            scale_factorx = blurred_img.shape[1]/zoomed_in_img.shape[1]
-            scale_factory = blurred_img.shape[0]/zoomed_in_img.shape[0]
-            scaled_boxes = []
-            for box in boxes:
-                scaled_boxes.append([box[0]*scale_factorx,box[1]*scale_factory,box[2]*scale_factorx,box[3]*scale_factory])
-            print('scaled_ground_boxes',scaled_boxes)
-            for true_box in ground_boxes:
-                for box in scaled_boxes:
-                    print('true_box',true_box)
-                    print('box',box)
-                    calculate_iou(true_box,box)
+
+
                     
                   
 
@@ -125,5 +104,10 @@ cv2.setMouseCallback("Grid", mouse_event)
 cv2.imshow("Grid", cv2.GaussianBlur(img, (21, 21), 0))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+# print('counts',counts)
+for i in range(len(counts)):
+    print(f' grid {i} detected {counts[i]} ')
+print('total',sum(counts))
+
 
 
